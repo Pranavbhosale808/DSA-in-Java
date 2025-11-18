2c7ec1b6e177c76887303496884bf6f9af17aed7
pranavbhosle808 <pranavbhosle04@gmail.com>
2025-11-19 00:09:11 +0530

    finded the second largest element and removed duplicated from the array

---
+++ Questions/Array/SecondLargest.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class SecondLargest {
+
+    public static int secondLargest(int arr[]){
+        int largest = arr[0];
+        int secondLargest = -1;
+        for(int i=0;i<arr.length;i++){
+            if(largest<arr[i]){
+                secondLargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>secondLargest ){
+                secondLargest=arr[i];
+            }
+        }
+        return secondLargest;
+    }
+
+    public static void removeDuplicateFromArray(int arr[]){
+        int i = 0;
+
+        for(int j=1;j<arr.length;j++){
+            if(arr[i]!=arr[j]){
+                arr[i+1] = arr[j];
+                i++;
+            }
+        }
+
+        System.out.println(i+1);
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = {1,1,3,1,2,3,4,1,2,3};
+        removeDuplicateFromArray(arr);
+        // int res = secondLargest(arr);
+        // System.out.println(res);
+        // int largest = arr[0];
+        // int secondLargest = -1;
+
+        // for(int i=1;i<arr.length;i++){
+        //     if(arr[i]>largest){
+        //         largest=arr[i];
+        //     }
+        // }
+
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]<largest && secondLargest<arr[i]){
+        //             secondLargest=arr[i];
+        //     }
+        // }
+
+        // System.out.println("Second Largest elemnt in array "+secondLargest);
+    }
+    
+}
